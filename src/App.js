c686f404dd742d88828f4eb15af53be67760456d
Ramakotaiah Vadlamudi <ramakotaiah12@gmail.com>
2021-05-06 09:45:10 +1000

    fifth commit: branch on second branch before merging
    >created branch on second branch before merging
    > update app.js file h1 element
    >h1 element is fifth commit, branch on second branch before merging

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     <div className="App">
       <h1>first commit</h1>
       <h1>second commit</h1>
+      <h1>fifth commit, branch on second branch before merging</h1>
     </div>
   );
 }
